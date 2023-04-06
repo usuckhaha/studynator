665b8e3efd949fdcbbfb143ca34426e8603c1ee1
usuckhaha <rick.esca05@gmail.com>
2023-04-06 18:36:25 -0400

    built project

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
+            // for you, but you can always add more by pressing Ctrl+F8
             System.out.println("i = " + i);
         }
     }
