f1033ed8e11f93a38ff340608d0b5a218cad2bda
usuckhaha <rick.esca05@gmail.com>
2023-04-06 19:39:50 -0400

    help

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ Author: @usuckhaha
 */
 public class Main {
     public static void main(String[] args) {
-        int i=4;
+        int i=5;
         System.println("Welcome to Studynator!!!\n\nReady to get Studynated?");
     }
 }
