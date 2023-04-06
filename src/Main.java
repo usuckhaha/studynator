910ef22f7f23a7dbbf2ee2068a31867ab34751b0
usuckhaha <rick.esca05@gmail.com>
2023-04-06 19:01:13 -0400

    built project

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-        }
+        int i=1;
+        System.println("aaaaaa");
     }
 }
